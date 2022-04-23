ee8b25f2f0376dd70779d01b190020d481952de7
StrongZhanglixiang <1102884786@qq.com>
2022-04-23 23:03:05 +0800

    splash更新代码

--- app/src/main/java/com/wan/android/MainActivity.java
+++ app/src/main/java/com/wan/android/MainActivity.java
@@ -2,15 +2,22 @@ package com.wan.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
     }
 }
