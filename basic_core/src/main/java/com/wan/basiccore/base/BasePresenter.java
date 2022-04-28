9cfabb926567a85d3116ebeaf6d6d361bb35a155
StrongZhanglixiang <1102884786@qq.com>
2022-04-28 23:51:34 +0800

    更新mvp代码

---
+++ basic_core/src/main/java/com/wan/basiccore/base/BasePresenter.java
@@ -0,0 +1,22 @@
+package com.wan.basiccore.base;
+
+import com.wan.basiccore.mvp.MvpPresenter;
+
+/**
+ * ================================================
+ * 作    者：zhanglx
+ * 版    本：1.0
+ * 创建日期：2022/4/28 23:49
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public class BasePresenter<V extends BaseView> extends MvpPresenter<V> {
+
+
+    @Override
+    public void attach(V baseView) {
+        super.attach(baseView);
+
+    }
+}
