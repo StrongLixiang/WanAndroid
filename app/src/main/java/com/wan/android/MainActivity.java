c67e1dd0f694ff3d13a003f08801c84164e47c25
StrongZhanglixiang <1102884786@qq.com>
2022-05-16 23:21:19 +0800

    update

--- app/src/main/java/com/wan/android/MainActivity.java
+++ app/src/main/java/com/wan/android/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        while (true) {
+            Log.d("zlx", "1111111111111");
+            SystemClock.sleep(3000);
+            Log.d("zlx", "22222222222");
+        }
+
+
     }
 
 
---
+++ app/src/main/java/com/wan/android/common/Constant.java
@@ -0,0 +1,16 @@
+package com.wan.android.common;
+
+/**
+ * ================================================
+ * 作    者：zhanglx
+ * 版    本：1.0
+ * 创建日期：2022/5/16 23:18
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public  class Constant {
+    public static final String PRIVACY_POLICY_URL = "https://goweii.gitee.io/wanandroidserver/doc/privacy_policy.html";
+    public static final String BROWSER_UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36";
+
+}
